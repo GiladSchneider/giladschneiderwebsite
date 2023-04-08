c8d1bcef8680578a67820bf2f33b0e71082fc3a5
Gilad Schneider <giladschneider2@gmail.com>
2023-04-08 11:25:31 -0400

    testing heroku

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = flask.Flask(__name__)
 
 # create a route
 @app.route('/')
-@app.route('/index')
 @app.route('/home')
-def index():
-    return flask.render_template('index.html')
+def home():
+    # return flask.render_template('index.html')
+    return 'Hello World'
 
 # run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
