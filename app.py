2e51de1330108d3586ac51ace53f7f3d2c6a2453
Gilad Schneider <giladschneider2@gmail.com>
2023-04-08 13:11:04 -0400

    improvements

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # import dependencies
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 # create a Flask app
 app = Flask(__name__)
 
 # create a home route
-@app.route('/', methods=['GET'])
-@app.route('/home', methods=['GET'])
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # get the main_image.jpeg file from the static folder
+    image_file = url_for('static', filename='images/main_image.jpeg')
+    return render_template('index.html', image_file=image_file)
 
 # create a research route
 @app.route('/research', methods=['GET'])
@@ -26,6 +28,10 @@ def projects():
 def games():
     return render_template('games.html')
 
+# create a cv route
+@app.route('/cv', methods=['GET'])
+def cv():
+    return render_template('cv.html')
 
 # run the app
 if __name__ == '__main__':
